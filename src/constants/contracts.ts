fb8572afbcd0a921e923d37cdb99789ebd9a3a69
nftmoondogs <nftmoondogs@gmail.com>
2023-07-05 19:14:49 +0530

    first commit

---
+++ src/constants/contracts.ts
@@ -0,0 +1,9 @@
+export const coreContracts: ContractsConfig = {
+  token: '0xAAf967756CEffa565C4d539e0306b3a281710d5D',
+  bridge: '0xAF0D3617E3d59A59E5fceA1ebF75b61dFC9B5E2B',
+}
+
+export const zkSyncContracts: ContractsConfig = {
+  token: '0xc83a5f45AA1954fF4D7157114DDBE72b1019f926',
+  bridge: '0x2eaE7754a0b614E906117aE442c38c702a1d0935',
+}
